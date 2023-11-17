0bc599583cedc33c44dbf0879b2231453aabc249
Gabriel Gomes <gabriel2011.time@hotmail.com>
2023-11-17 18:53:01 -0300

    feat: add btrees

--- utils/generate-new-b-trees.py
+++ utils/generate-new-b-trees.py
@@ -8,9 +8,9 @@ file_name = join(current_dir, "./db/users.csv")
 b_tree_data = read_file_and_build_tree(file_name, 10000)
 b_tree_data = b_tree_data.save_to_file(join(current_dir, "user_btree.json"))
 ## books
-file_name = join(current_dir, "./db/books.csv")
-b_tree_data = read_file_and_build_tree(file_name, 10000)
-b_tree_data = b_tree_data.save_to_file(join(current_dir, "rating_btree.json"))
+# file_name = join(current_dir, "./db/books.csv")
+# b_tree_data = read_file_and_build_tree(file_name, 10000)
+# b_tree_data = b_tree_data.save_to_file(join(current_dir, "rating_btree.json"))
 # ratings
 file_name = join(current_dir, "./db/ratings.csv")
 
